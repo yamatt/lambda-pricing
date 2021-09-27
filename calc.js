08384a3f7a67dab9463fdd6a727edd77e6ff2282
Matt Copperwaite <matt@copperwaite.net>
2021-09-27 10:43:57 +0100

    run on window load

--- calc.js
+++ calc.js
@@ -1,7 +1,6 @@
 const calcform_el = document.querySelector("#calc");
 
-calcform_el.addEventListener("load", (e) => {
-  console.log("not run")
+window.addEventListener("load", (e) => {
   const querystring = new URLSearchParams(window.location.search);
   for (let key in querystring) {
     calcform_el.elements[key].value = querystring.get(key)
